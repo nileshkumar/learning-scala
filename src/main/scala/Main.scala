1052dc47ddda0df0ae2ed9f90863471664230cef
Nilesh Kumar <nileshkumar.be@gmail.com>
2023-11-17 16:32:11 +0100

    add patern matching case statements

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,9 @@
   println(greetMe("John"))
   println(partyAtHome(5, false))
   println(partyAtHome(5, true))
+  println(competition(1))
+  println(competition(10))
+  println(competition(3))
 
 def msg = "I was compiled by Scala 3. :)"
 def sum(num1: Int, num2: Int): Int = num1 + num2
@@ -19,3 +22,9 @@ def greetMe(name: String): String = s"Hello $name!"
 def partyAtHome(guests: Int, tentAvailable: Boolean): String =
   if guests < 10 && tentAvailable then "Let's party hard.... ;) "
   else "No party :( "
+
+def competition(result: Int): String = result match
+  case 1 => "Gold!!!"
+  case 2 => "Silver!!"
+  case 3 => "Bronze!"
+  case _ => "Good Luck next time."
