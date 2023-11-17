b6f0e23ae8d7b4a16e980cbe2c755cef349ce3c9
Nilesh Kumar <nileshkumar.be@gmail.com>
2023-11-17 12:11:28 +0100

    add add fumction

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 @main def hello: Unit =
   println("Hello world!, I am learning Scala :) ")
   println(msg)
+  println(sum(4,5))
 
 def msg = "I was compiled by Scala 3. :)"
+def sum(num1: Int, num2: Int): Int = num1 + num2
