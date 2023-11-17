1dab4200eb1d84cd4baaa01956646d843c8d280d
Nilesh Kumar <nileshkumar.be@gmail.com>
2023-11-17 16:02:42 +0100

    add more examples for named parameter fumction

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,11 @@
   println(turnOnLight(true))
   println(turnOnLight(false))
   println(turnOnLight())
+  println(greetMe("John"))
 
 def msg = "I was compiled by Scala 3. :)"
 def sum(num1: Int, num2: Int): Int = num1 + num2
 
 def turnOnLight(switchOn: Boolean = false): String = if switchOn then "Lights on!" else "Lights off!"
+
+def greetMe(name: String): String = s"Hello $name!"
