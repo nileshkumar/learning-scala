ecf484aaa2ad663e9ee8b1ac7c5031c79c0f847e
Nilesh Kumar <nileshkumar.be@gmail.com>
2023-11-17 15:48:07 +0100

    add named parameter fumction

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,11 @@
   println("Hello world!, I am learning Scala :) ")
   println(msg)
   println(sum(4,5))
+  println(turnOnLight(true))
+  println(turnOnLight(false))
+  println(turnOnLight())
 
 def msg = "I was compiled by Scala 3. :)"
 def sum(num1: Int, num2: Int): Int = num1 + num2
+
+def turnOnLight(switchOn: Boolean = false): String = if switchOn then "Lights on!" else "Lights off!"
